9be9018f3f2f0f7974aa0b8cf7b2e0149d4d5e67
unknown <nguyenbaohoa02@gmail.com>
2024-09-21 11:47:58 +0700

    comple UI

--- src/Component/Rester/Signin.jsx
+++ src/Component/Rester/Signin.jsx
@@ -1,55 +1,114 @@
 import { Box, Button, FormControl, FormErrorMessage, Input } from '@chakra-ui/react'
 import { Field, Form, Formik } from 'formik'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import * as Yup from "yup"
+import { auth, provider } from './Firebase';
+import { signInWithPopup } from 'firebase/auth'
 
-const validationShema = Yup.object().shape({
+const validationSchema = Yup.object().shape({
     email: Yup.string().email("Invalid email address").required("Required"),
     password: Yup.string().min(8, "  Password must be at least 8 characters").required("Password is required")
 })
 const Signin = () => {
     const initialValues = { email: "", password: "" };
+    const navigation = useNavigate()
     const handleSubmit = (values) => {
         console.log("values:", values)
     }
+    const handleNavigation = () => navigation("/signup")
+    const handleLogin = async () => {
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
-            <div className='border'>
-                <Box p={8} display={"flex"} flexDirection={'column'} alignItems={"center"}>
-                    <img className="mb-5" src="./KoLLpWDb4f6.png" alt="" />
-                    <Formik
-                        validationSchema={validationShema}
-                        initialValues={initialValues}
-                        onSubmit={handleSubmit}>
-                        {(formikProps) => (<Form className='space-y-8'>
+            <div>
+                <div className='border border-gray-300 p-6 rounded-lg mb-5 w-full'>
+                    <Box p={6} display={"flex"} flexDirection={'column'} alignItems={"center"}>
+                        <img className="mb-6" src="./logoig.png" alt="Instagram Logo" />
+                        <Formik
+                            initialValues={initialValues}
+                            onSubmit={handleSubmit}
+                            validationSchema={validationSchema}>
+                            {(formikProps) => (
+                                <Form className='space-y-8'>
+                                    <Field name="email">
+                                        {({ field, form }) => (
+                                            <FormControl isInvalid={form.errors.email && form.touched.email}>
+                                                <Input className="w-full border border-gray-300" {...field} id='email' placeholder='Mobile Number or Email' />
+                                                <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
 
-                            <Field name="email">
-                                {({ field, form }) => (<FormControl isInvalid={form.errors.email && form.touched.email}>
-                                    <Input className="w-full" {...field} id='email' placeholder='Mobile Number or Email'>
-                                    </Input>
-                                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                                </FormControl>)}
+                                    <Field name="password">
+                                        {({ field, form }) => (
+                                            <FormControl isInvalid={form.errors.password && form.touched.password}>
+                                                <Input className="w-full border border-gray-300" {...field} id='password' placeholder='Password' type='password' />
+                                                <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Button
+                                        mt={4}
+                                        type='submit'
+                                        isLoading={formikProps.isSubmitting}
+                                        css={{
+                                            width: '100%',
+                                            border: '1px solid',
+                                            padding: '0.5rem', // py-2
+                                            borderRadius: '0.5rem', // rounded-lg
+                                            color: 'white',
+                                            fontSize: '1rem', // text-base
+                                            backgroundColor: 'rgba(14, 165, 233, 0.75)', // bg-sky-500/75
+                                            ":hover": {
+                                                backgroundColor: 'rgba(14, 165, 233, 1)', // Thay đổi màu nền khi hover
+                                                opacity: 1, // Đặt độ trong suốt khi hover
+                                            },
+                                        }}
+                                    >
+                                        Log In
+                                    </Button>
+                                    <div className="flex items-center my-4">
+                                        <div className="flex-grow border-t border-gray-300"></div>
+                                        <span className="mx-4 text-gray-500">OR</span>
+                                        <div className="flex-grow border-t border-gray-300"></div>
+                                    </div>
 
-                            </Field>
+                                    <div className="flex justify-center">
+                                        <button
+                                            onClick={handleLogin}
+                                            className="flex items-center justify-center w-full bg-white py-2 px-4 text-blue-500 hover:shadow-lg transition duration-200"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="currentColor"
+                                                className="w-5 h-5 mr-2"
+                                            >
+                                                <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.35v21.3C0 23.4.6 24 1.35 24h21.3c.75 0 1.35-.6 1.35-1.35V1.35C24 .6 23.4 0 22.675 0zm-2.675 12.17h-3.34v10.66h-4.24V12.17h-2.71v-4.07h2.71V6.91c0-3.08 1.79-4.76 4.45-4.76 1.27 0 2.58.22 2.58.22v2.8h-1.45c-1.43 0-1.87.88-1.87 1.77v2.11h3.34l-.53 4.07z" />
+                                            </svg>
+                                            Đăng nhập với Facebook
+                                        </button>
+                                    </div>
+                                    <p className='text-center text-sm py-2'>Forgot password?</p>
+                                </Form>
+                            )}
+                        </Formik>
 
-                            <Field name="password">
-                                {({ field, form }) => (<FormControl isInvalid={form.errors.password && form.touched.password}>
-                                    <Input className="w-full" {...field} id='password' placeholder='Password'>
-                                    </Input>
-                                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                                </FormControl>)}
-
-                            </Field>
-                            <p className='text-center'>People who use our service may have uploaded your contact information to Instagram</p>
-                            <p className="text-center">By signing up, you agree to our Terms , Privacy Policy and Cookies Policy .</p>
-                            <Button className='w-full' mt={4} colorScheme='blue' type='submit' isLoading={formikProps.isSubmitting}>Sign In</Button>
-                        </Form>)}
-
-                    </Formik>
-                </Box>
-            </div>
-            <div className='border w-full border-slate-300 mt-5'>
-                <p className='text-center py-2'>If You Don't Have Account <span className='ml-2 text-blue-700 cursor-pointer'> Sign Up</span></p>
+                    </Box>
+                </div>
+                <div className='mt-5 border border-gray-300 p-4 rounded-lg w-full'>
+                    <p className='text-center py-2'>
+                        If You Don't Have an Account
+                        <span onClick={handleNavigation} className='ml-2 text-blue-700 cursor-pointer'>Sign Up</span>
+                    </p>
+                </div>
             </div>
         </div>
     )
--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Auth.css"
 import Signin from '../../Component/Rester/Signin'
+import { useLocation } from 'react-router-dom'
+import Signup from '../../Component/Rester/Signup'
 const Auth = () => {
+    const location = useLocation();
     return (
-        <div>
-
-            <div className='flex items-center justify-center h-[100vh]'>
+        <div className='my-8'>
+            <div className='flex items-start justify-center h-[100vh] space-x-5'>
                 <div className='relative hidden lg:block'>
                     <div className="h-[35.3rem] w-[23rem]">
                         <img className="h-full w-full" src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" alt="" />
@@ -16,8 +18,8 @@ const Auth = () => {
                     </div>
                 </div>
 
-                <div className='w-[23vw]'>
-                    <Signin />
+                <div className='w-[40vw] lg:w-[23vw]'>
+                    {location.pathname === '/login' ? <Signin/> : <Signup/>}
                 </div>
             </div>
         </div>
